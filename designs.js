2c24bf78e2f3489df03837daa386847f793149eb
Sérgio <sergio.m.rib@gmail.com>
2018-01-16 14:49:40 +0000

    Hover effects over the instructions and the cells.
    Change of the fonts, sizes and colors.

--- designs.js
+++ designs.js
@@ -3,17 +3,24 @@
 $("#sizePicker").submit(function (event) {
     event.preventDefault(); // prevent refreshing the page
     $("#pixel_canvas").children().remove()
-    var height = $("#input_height").val();
-    var width = $("#input_width").val();
-    console.log("The height is:", height)
-    console.log("The width is:", width)
-    makeGrid(height, width);
+    var canvasHeight = $("#input_height").val(),
+    canvasWidth = $("#input_width").val(),
+    cellSize = $("#input_cell_size").val();
+
+    console.log("The height is:", canvasHeight)
+    console.log("The width is:", canvasWidth)
+    console.log("The cell size is:", cellSize, "px")
+
+    makeGrid(canvasHeight, canvasWidth);
+    $("table").toggleClass("with-no-canvas")
+    $("#sizePicker").hide();
+    $("#createNewGrid").show();
 
 })
 
 function makeGrid(height, width) {
     /* 
-    function that gets the arguments for the grid
+    function that gets the arguments for the grid,
     creates the html syntax and appends it to "pixel_canvas"
     */    
     var columns, table;
@@ -44,22 +51,40 @@ function painter(cell, color) {
     Declared separately to be possible to pass arguments.
     The arguments are a cell - jquery selector of the cell - 
     and the color - the value of the color given by the object 
-    return by the colorPicker change event
+    returned by the colorPicker change event
     */
-    console.log("clicked")
+    console.log("Painter function worked")
     cell.css("background-color", color)
 }
 
-$("table").on("click","td", function () {
+var mouseDown = false;
+
+$("table").on({
+    mousedown: function () {
+        mouseDown = true;
+    },
+    mouseup: function (){
+        mouseDown = false;
+    }
+})
+
+$("table").on("mouseover","td", function () {
     /* event that selects the cell and passes it to the painter
     function
     */
-    painter($(this), selectedColor.val());
+    if (mouseDown == true){
+        console.log("clicked");
+        painter($(this), selectedColor.val());
+    }
 })
 
 $("table").on("dblclick","td", function () {
     // function to erase color (change it to white!)
     console.log("clicked")
-    $(this).css("background-color", "#FFF");  
+    $(this).css("background-color", "transparent");  
 })
 
+$("#createNewGrid").on("click", function () {
+    $("#sizePicker").show();
+    $("#createNewGrid").hide();
+})
