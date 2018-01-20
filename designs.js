07017ef27274c869730678cf4b56892f521d1604
Sérgio <sergio.m.rib@gmail.com>
2018-01-20 03:43:44 +0000

    type: feat docs
    New: drawing and erasing have specific cursors;
    Major alterations to the docs. Using Udacity's guidelines
    Click activates drawing;
    Doubleclick activates/deactivates erasing;
    Click deactivates both drawing and erasing.

--- designs.js
+++ designs.js
@@ -1,111 +1,163 @@
 // Select size input
 
-$("#sizePicker").submit(function (event) {
-    event.preventDefault(); // prevent refreshing the page
-    $("#pixel_canvas").children().remove()
-    var canvasHeight = $("#input_height").val(),
-    canvasWidth = $("#input_width").val(),
-    cellSize = $("#input_cell_size").val();
+$('#sizePicker').submit(function (event) {
 
-    console.log("The height is:", canvasHeight)
-    console.log("The width is:", canvasWidth)
-    console.log("The cell size is:", cellSize, "px")
+    // prevent refreshing the page
+    event.preventDefault();
 
+    // remove the previous table
+    $('#pixel_canvas').children().remove()
+
+    // assign form values to variables
+    var canvasHeight = $('#input_height').val(),
+        canvasWidth = $('#input_width').val();
+
+    // debugging logs...
+    console.log('The height is:', canvasHeight)
+    console.log('The width is:', canvasWidth)
+
+    // callback to makeGrid function - creates the canvas
     makeGrid(canvasHeight, canvasWidth);
-    $("table").toggleClass("with-no-canvas")
-    $("#sizePicker").hide();
-    $("#createNewGrid").show();
+
+    $('table').toggleClass('with-no-canvas')
+
+    // hiding the size picker and showing the createNewGrid button
+    $('#sizePicker').hide();
+    $('#createNewGrid').show();
 
 })
 
 function makeGrid(height, width) {
-    /* 
-    function that gets the arguments for the grid,
-    creates the html syntax and appends it to "pixel_canvas"
-    */    
-    var columns, table;
+    /*
+    * @description creates the grid - creates the html syntax and appends it to 'pixel_canvas'
+    * @param {number} height
+    * @param {number} width
+    */
+    let columns, table;
+
+    // create number of cells (columns) per line
     for(i = 0; i < width; i++) {
-        columns = columns + "<td></td>"; 
+        columns = columns + '<td></td>';
     }
+
+    // creates number of lines using
     for(i = 0; i < height; i++) {
-        table = table + "<tr>" + columns + "</tr>";
+        table = table + '<tr>' + columns + '</tr>';
     }
-    $("#pixel_canvas").append(table)
-    console.log("added the grid")
-}   
+    $('#pixel_canvas').append(table)
+    console.log('added the grid')
+}
 
 
-/* 
+/*
 Selection of the color for cells
 and aplying it to them
 */
-var selectedColor = "#000";
-selectedColor = $("#colorPicker").on("change", function(){
-     // event to select color
-     var colorCode = $(this).val(),
-     styleTag = " style=\"background-color:" + colorCode + ";\">",
-     textToInsert = "<button class=\"reselect-button\""+ styleTag + colorCode + "</button>";
-     $("#selected-colors-list").append(textToInsert);     
-     console.log($(this).val());
+
+
+
+let selectedColor = $('#colorPicker').on('change', function(){
+    // event to select color
+
+    // assign the color code to the variable
+    let colorCode = $(this).val(),
+
+    // add a button with the selected color code and background to the bottom div
+    styleTag = ' style="background-color:' + colorCode + '";>',
+    textToInsert = '<button class="reselect-button"'+ styleTag + colorCode + '</button>';
+    $('#selected-colors-list').append(textToInsert);
+
+    // debugging
+    console.log($(this).val());
 })
 
 
-// to reselect one of the colors already used
-$("selected-colors-list").on("click",".reselect-button", function (){
-    console.log("reselection worked. Color code is: " + $(this).css("background-color"));
-    selectedColor.val() = "#000";
+// to reselect one of the colors already used -- DOESN'T WORK
+$('selected-colors-list').on('click','.reselect-button', function (){
+    /* Still not working!
+    This event doesn't react to dinamically created buttons.
+    Why?
+    */
+    console.log('Reselection worked. Color code is: ' + $(this).css('background-color'));
+    selectedColor.val() = '#000';
 })
 
 function painter(cell, color) {
     /*
-    This function changes the color of the cell.
-    Declared separately to be possible to pass arguments.
-    The arguments are a cell - jquery selector of the cell - 
-    and the color - the value of the color given by the object 
-    returned by the colorPicker change event
+    * @description changes the color of the cell. Declared separately to be possible to pass arguments.
+    * @param a cell - jquery selector of the cell -
+    * @param {sring} the color - the value of the color given by the object returned by the colorPicker change event
     */
-    console.log("Painter function worked")
-    cell.css("background-color", color)
+    console.log('Painter function worked')  // debugging purpose
+    cell.css('background-color', color)
 }
 
-var paintCells = false,
-    eraseCells = false;
+let isDrawing = false,
+    isErasing = false;
 
-$("table").on({
+$('table').on({
+    /*
+    * @description using click and doubleclick to toggle painting and isErasing
+    */
     click: function () {
-        if (paintCells == false) {
-            paintCells = true;
-            eraseCells = false;
-        }else{
-            paintCells = false;
+
+        // if either drawing or erasing feature is ON it disables it
+        if (isDrawing == true || isErasing == true) {
+            isDrawing = false;
+            isErasing = false;
+
+            // changing the cursor default
+            $(this).css('cursor', 'default')
+        }else {
+
+            // If neither are ON then the user wants to start drawing
+            isDrawing = true;
+
+            //return cursor to to show it's drawing
+            $(this).css('cursor', 'crosshair')
         }
     },
     dblclick: function () {
-        if (eraseCells == false) {
-            eraseCells = true;
-            paintCells = false;
-        }else{
-            eraseCells = false;
+
+        // Start erasing
+        if (isErasing == false) {
+            isErasing = true;
+            isDrawing = false;
+
+            // change the cursor to show it's erasing
+            $(this).css('cursor','pointer')
+        } else {
+            isErasing = false;
+
+            // change cursor to default
+            $(this).css('cursor','default')
         }
     }
 })
 
-$("table").on("mouseover","td", function () {
+$('table').on('mouseover','td', function () {
     /* event that selects the cell and passes it to the painter
     function
     */
-    if (paintCells == true){
-        console.log("clicked");
+
+    // if statement to draw
+    if (isDrawing == true){
+        console.log('clicked');
         painter($(this), selectedColor.val());
     }
-    if (eraseCells == true) {
-        console.log("erasing color");
-        $(this).css("background-color", "transparent");
+
+    // if statement to erase
+    if (isErasing == true) {
+        console.log('isErasing color');
+        $(this).css('background-color', 'transparent');
     }
 })
 
 
-$("#createNewGrid").on("click", function () {
-    $("#sizePicker").show();
-    $("#createNewGrid").hide();
+$('#createNewGrid').on('click', function () {
+    /*
+    * @description event to show the creation grid form
+    */
+    $('#sizePicker').show();
+    $('#createNewGrid').hide();
 })
