d01f31d5b06e5fea9bfbd92aa888bdb9d8d0c33b
Sérgio <sergio.m.rib@outlook.com>
2018-04-04 17:17:08 +0100

    Major alterations to game mechanics
    -Clicked cards are stored in an array;
    -This array is used to track number of cards clicked and trigger the comparison
    - Moved shuffle function to the end of file
    - event listeners are closer (for easy-to-read purpose)
    - declared variables are now at the top (for easy-to-read purpose)

--- js/memory_card_game.js
+++ js/memory_card_game.js
@@ -8,15 +8,16 @@
 const repeatButton = document.getElementById('restart');
 let allCards = document.getElementsByClassName('card');
 let theDeck = document.getElementById('the-deck');
+let clickedCards = [];
 let count = 0;
 /*
  * the deck is shuffled for the first time when document loads
 */
 
 
-let allCards2 = shuffle(allCards);
-for(let i = 0; i < allCards2.length; i ++) {
-    theDeck.appendChild(allCards2[i]);
+allCards = shuffle(allCards);
+for(let i = 0; i < allCards.length; i ++) {
+    theDeck.appendChild(allCards[i]);
 };
 
 repeatButton.addEventListener('click', function (){
@@ -31,8 +32,77 @@ repeatButton.addEventListener('click', function (){
         theDeck.appendChild(allCards2[i]);
         allCards2[i].classList.remove('match', 'show', 'open');
     };
+    clickedCards = [];
 });
 
+
+
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
+/*
+ * Event listeners for the cards
+*/
+
+
+theDeck.addEventListener('click', function (event) {
+    /*
+     * Gets the card element clicked (the target)
+     * toggles "show" and "open" classes on that element
+    */
+
+
+    console.log(clickedCards);
+
+    if (clickedCards.length < 2) {
+        // if clickedCards already has 1 card it adds another; if it has 2 cards it wont add any other
+        clickedCards.push(event.target);
+        event.target.classList.add('open', 'show');
+    };
+
+    if (clickedCards.length === 2) {
+        moveCounter();
+        comparator(clickedCards[0], clickedCards[1]);
+        console.log('2 clicked cards')
+    };
+
+});
+
+function comparator(firstCard, secondCard) {
+    /*
+    * @description compares the two cards information
+    * Calls resetCards function with 1 second delay
+    */
+    if (firstCard.children[0].classList.value === secondCard.children[0].classList.value) {
+        firstCard.classList.toggle('match');
+        secondCard.classList.toggle('match');
+        window.setTimeout(resetCards, 1000);
+    } else {
+        window.setTimeout(resetCards, 1000);
+    };
+};
+
+function resetCards() {
+    // removes the classes concerning visibility
+    clickedCards[0].classList.remove('open', 'show');
+    clickedCards[1].classList.remove('open', 'show');
+    clickedCards= [];
+};
+
+
+function moveCounter() {
+    count++;
+    document.getElementById('move-counter').textContent = count;
+};
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -52,81 +122,4 @@ function shuffle(array) {
     };
 
     return array;
-};
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-/*
- * Event listeners for the cards
-*/
-
-let card1, card1_classes, card2, card2_classes;
-
-
-theDeck.addEventListener('click', function (event) {
-    /*
-     * Gets the card element clicked (the target)
-     * toggles "show" and "open" classes on that element
-    */
-
-    event.target.classList.add('open', 'show');
-
-    // Assignment of clicked elements to variables
-    if (card1 == undefined) {
-        // get the first card info (class name)
-        card1 = event.target;
-        card1_classes = event.target.children[0].className;
-    } else {
-        /*
-         * get the second card info if the first exists
-         * call the comparison function
-        */
-        card2 = event.target;
-        card2_classes = event.target.children[0].className;
-        comparator(card1_classes, card2_classes);
-        moveCounter();
-    };
-});
-
-function comparator(firstCard, secondCard) {
-    /*
-    * @description compares the two cards information
-    * Calls resetCards function with 1 second delay
-    */
-    if (firstCard == secondCard) {
-        card1.classList.toggle('match');
-        card2.classList.toggle('match');
-        eraseCards();
-    } else {
-        window.setTimeout(resetCards, 1000);
-    };
-};
-
-function resetCards() {
-    // removes the classes concerning visibility
-    card1.classList.remove('open', 'show');
-    card2.classList.remove('open', 'show');
-    eraseCards();
-};
-
-function eraseCards() {
-    // erases variable info
-    card1 = undefined;
-    card2 = undefined;
-};
-
-function moveCounter() {
-    count++;
-    console.log(count);
-    document.getElementById('move-counter').textContent = count;
 };
